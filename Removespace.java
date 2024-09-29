a8a77f5406024cc6118444d427e66256117e7524
KANITA VERMA <vermakanita13@gmail.com>
2024-09-29 23:15:22 +0530

    Add files via upload

---
+++ Removespace.java
@@ -0,0 +1,17 @@
+//Remove Space
+
+
+public class Removespace{
+public static void main(String[]args){
+String str = " i am iris ";
+String newstr="";
+for(int i=0;i<str.length();i++){
+if(str.charAt(i)!=' '){
+
+newstr+= str;
+}}
+//str=newstr;
+System.out.println(newstr);}}
+
+
+
