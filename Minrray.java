35fe8f308163a7df4235696c2561dfd5a18f12fa
KANITA VERMA <vermakanita13@gmail.com>
2024-09-30 21:45:18 +0530

    Add files via upload

---
+++ Minrray.java
@@ -0,0 +1,21 @@
+//MIN Array Element
+
+
+import java.util.*;
+public class Minrray{
+public static void main(String []args){
+Scanner sc = new Scanner (System.in);
+System.out.println("Enter nu");
+int n= sc.nextInt();
+int arr[] = new int[n];
+for(int i=0;i<n;i++){
+arr[i]= sc.nextInt();
+}
+int min = n;
+for(int i=0;i<n;i++){
+if(min>arr[i]){
+min=arr[i];
+}}
+System.out.println(min);
+}}
+
