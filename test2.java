d5d1aae90f3c2768272566ffe32557a567d3afe4
KANITA VERMA <vermakanita13@gmail.com>
2024-10-01 22:27:20 +0530

    Add files via upload

---
+++ test2.java
@@ -0,0 +1,30 @@
+class human {
+
+    public String name1;
+    public int age1;
+    public String name2;
+    public int age2;
+
+
+    human(String name1 , String nama2 , int age1, int age2){
+        this.name1= name1;
+        this.name2= name2;
+        this.age1= age1;
+        this.age2= age2;
+    }
+    public int  compareto(){
+        return 0;
+    }
+   
+    
+}
+class person extends human{
+    public int  p1(String name1,int age1){
+        return name1+" "+age1;
+
+    }
+
+    public int p2(String name2,int age2){
+        return 
+    }
+}
