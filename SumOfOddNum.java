59b96f20a1d9fa15c4b385fa379a6acaa834c5ef
KANITA VERMA <vermakanita13@gmail.com>
2024-10-01 22:26:51 +0530

    Add files via upload

---
+++ SumOfOddNum.java
@@ -0,0 +1,18 @@
+//Sum Of Odd Numbers
+
+public class SumOfOddNum{
+
+public static void main(String []args){
+int arr[]= {3,6,7,9,4};
+int sum=0;
+for(int i=0;i<arr.length;i++){
+if(i%2==0){
+sum = sum+ arr[i];
+
+//for(int i=0;i<arr.length;i++){
+System.out.print(arr[i]);
+
+}}
+}}
+
+
