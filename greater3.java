35898cbe672f907762d915a0008aa55cec0a03fd
KANITA VERMA <vermakanita13@gmail.com>
2024-10-02 20:11:59 +0530

    Add files via upload

---
+++ greater3.java
@@ -0,0 +1,20 @@
+package _2Assignments29Q;
+import java.util.Scanner;
+
+public class greater3{
+    public static void main(String args[]){
+    int a=3;
+    int b=9;
+    int c =59;
+    if(a>b&&a>c){
+    System.out.println("A");
+    }
+    else if(b>a&&b>c){
+    System.out.println("b");
+    }
+    
+    else{
+    System.out.println("c");
+    }
+    }}
+    
