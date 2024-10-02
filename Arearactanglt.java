6610f042a8b0a01eef14c2eb8b6bc685e7e14e54
KANITA VERMA <vermakanita13@gmail.com>
2024-10-02 20:11:10 +0530

    Add files via upload

---
+++ Arearactanglt.java
@@ -0,0 +1,13 @@
+package _2Assignments29Q;
+
+ // Area of rectangle
+
+import java.util.Scanner;
+public class Arearactanglt{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+System.out.println("enter height and width");
+int heigth = sc.nextInt();
+int width = sc.nextInt();
+ System.out.println(heigth*width);
+}}
