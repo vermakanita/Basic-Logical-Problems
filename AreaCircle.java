ae53c3d77a23c361164ac2d4e64d1c8128e70ff4
KANITA VERMA <vermakanita13@gmail.com>
2024-10-02 20:10:48 +0530

    Add files via upload

---
+++ AreaCircle.java
@@ -0,0 +1,16 @@
+ 
+ package _2Assignments29Q;
+ //Area of circle
+
+import java.util.Scanner;
+
+public class AreaCircle
+{
+public static void main(String []args)
+{  
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the radius");
+int r = sc.nextInt();
+System.out.println(3.14*r*r);
+}}
+  
