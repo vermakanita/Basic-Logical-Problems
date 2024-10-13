5989e69962f4b113935fb63b3fd5f1246c83292f
KANITA VERMA <vermakanita13@gmail.com>
2024-10-13 23:39:51 +0530

    Add files via upload

---
+++ WithSwapping.java
@@ -0,0 +1,14 @@
+package _2Assignments29Q;
+import java.util.Scanner;
+
+public class WithSwapping {
+    public static void main(String args[]){
+    int a = 5;
+    int b=10;
+    int c;
+    c=a;
+    a=b;
+    b=c;
+    System.out.println(a+" "+ b);
+    }}
+    
