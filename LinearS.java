19bfc2e39a6a9016836ea00a513f3fc2180c81ca
KANITA VERMA <vermakanita13@gmail.com>
2024-10-16 20:34:14 +0530

    Add files via upload

---
+++ LinearS.java
@@ -0,0 +1,26 @@
+package Search;
+
+public class LinearS
+ {
+    public static void main(String[] args){
+        int arr[]={4,2,7,8,1,2,5};
+        int size= 7;
+        int target = 50;
+        
+     
+        for(int i=0;i<size;i++)
+        {
+            if(arr[i]==target)
+            {
+                System.out.println("kk");
+            }
+
+        }
+        System.out.println(target);
+    
+
+         
+    }
+}
+    
+
