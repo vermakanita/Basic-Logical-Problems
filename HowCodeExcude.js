d6f38abaadcb9e8ef358f81fbd03e854a9e7cac7
KANITA VERMA <vermakanita13@gmail.com>
2024-11-07 23:20:44 +0530

    Add files via upload

---
+++ HowCodeExcude.js
@@ -0,0 +1,10 @@
+globle context excute //this 
+gec 
+function excute context
+eval excute context
+
+2 phashe 1 memory creation pashe // memeory alocation
+var=undefined
+fun=defination
+2 excution phase
+
