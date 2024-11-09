6d32e0917b6038c87222ea4f3f7d9aa8feb08a43
KANITA VERMA <vermakanita13@gmail.com>
2024-11-09 23:48:50 +0530

    Add files via upload

---
+++ quetion.js
@@ -0,0 +1,7 @@
+//1. data type need nhi h 
+// dynamicli tped HTMLDetailsElement
+// light weigth:run kren ke liye dushre platform ki need nhi engin se run ho jayega 
+// interpreted:line by line excutation
+// dependent lanuage 
+
+ 
