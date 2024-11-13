21b26a2b0ab68cc9a11e92080f2c6d327493f153
KANITA VERMA <vermakanita13@gmail.com>
2024-11-13 23:58:20 +0530

    Add files via upload

---
+++ _3.js
@@ -0,0 +1,16 @@
+function check (){
+    let str="kanita";
+    let alphabate=0;
+    let symbol=0;
+    let number=0;
+    for(let i=0;i<str.length;i++){
+        let char = str.charAt(i);
+        if((char >=65 && char <=90) ||(char >=97 && char <=122)){
+            alphabate++;
+        }
+        else if (char=='#'||char=='@'||char=='%'||char=='$'){
+            symbol++;
+        }
+        else if (char )
+    }
+}
