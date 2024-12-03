39a57283a825f905ca38dca0955da3eba25db387
KANITA VERMA <vermakanita13@gmail.com>
2024-12-03 22:34:23 +0530

    Add files via upload

---
+++ Add.js
@@ -0,0 +1,34 @@
+// function addE(){
+
+//     let arr=["ka","ve","ri","ka"];
+//      arr.push("mahima");// add last 
+//      console.log(arr);
+
+//      arr.unshift("kanita");
+//      console.log(arr);
+
+     
+// }addE();
+
+
+
+// function remove(){
+//     let k=["kanita","priya","mahima"];
+//     console.log(k);
+
+//     k.pop();
+//     console.log(k);
+//     k.shift();
+//     console.log(k);
+// } remove();
+
+
+
+
+
+
+function check(){
+    let l=["ka","va","ma","na"];
+    z.isArray("ka");
+    console.log(l);
+}
