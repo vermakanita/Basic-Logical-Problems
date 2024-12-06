20d75eaddf4e96cc9cecfe7c97c0a7ee5f764685
KANITA VERMA <vermakanita13@gmail.com>
2024-12-06 23:24:18 +0530

    Add files via upload

---
+++ First.js
@@ -0,0 +1,28 @@
+let arr=[10,20,30,40,{"name":"kanita"}];
+let x=arr.push(5);//
+console.log(arr);
+let y = arr.pop();
+console.log(y)
+
+// console.log(x);
+console.log(arr[4]);
+arr[4]="verma";
+console.log(x);
+console.log(arr)
+
+
+// let l= arr.splice(1,2,3,4);
+// console.log(l);
+// console.log(arr);
+
+
+
+
+// let p= arr.slice(1,4);
+// console.log(p);
+// console.log(arr);
+
+
+let p= arr.map(b => * 2);
+console.log(p);
+console.log(arr);
