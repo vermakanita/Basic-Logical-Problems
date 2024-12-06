fff00bd42c589a6bd90b91ce2d27c2e9fafbed3f
KANITA VERMA <vermakanita13@gmail.com>
2024-12-06 23:23:44 +0530

    Add files via upload

---
+++ Loop.js
@@ -0,0 +1,8 @@
+let arr = ["a", "b", "c"];
+for (let i = 0; i < arr.length; i++) {
+//   console.log(i); // Output: a, b, c
+}
+
+for (let value in arr) {
+  console.log(value); // Output: a, b, c
+}
