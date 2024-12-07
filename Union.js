ebb98d93ade4cb08a28b490d7884cf075c70dc51
KANITA VERMA <vermakanita13@gmail.com>
2024-12-07 15:44:45 +0530

    Add files via upload

---
+++ Union.js
@@ -0,0 +1,12 @@
+let union1=[2,3,4,5,6];
+let union2=[2,7,8,9,3,6];
+for(let i=0;i<union1.length;i++){
+for(let j=0;j<union2.length;j++){
+    if(union1[i]===union2[j]){
+        // var x=union1[i];
+        // console.log(x);
+        console.log(union1[i])
+    }
+}
+
+}
