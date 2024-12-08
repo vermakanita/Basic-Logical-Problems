a515e46ff0de86db426be020dc7e228607185929
KANITA VERMA <vermakanita13@gmail.com>
2024-12-08 19:06:35 +0530

    Add files via upload

---
+++ Q2.js
@@ -0,0 +1,7 @@
+// 2.If the given input is an array of numbers, return the sum of all the positives ones. 
+// If the array is empty or there aren't any positive numbers, return 0.
+
+// const input = [1, -4, 12, 0, -3, 29, -150];
+// output :- 42;	
+const input = [1, -4, 12, 0, -3, 29, -150];
+
