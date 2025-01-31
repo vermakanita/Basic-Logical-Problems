09b7867aa0c6d93b0c9fb986c4807c6e67ef3cc7
KANITA VERMA <vermakanita13@gmail.com>
2025-01-31 23:57:41 +0530

    Add files via upload

---
+++ MaxElement.js
@@ -0,0 +1,21 @@
+let arr =[5,1, 2, 3,6, 4,8, 5];
+let max = arr[0];
+// console.log(max);
+
+// for(let i=0;i<arr.length;i++){
+    // if(arr[i]>arr[i+1]){
+    //     max=arr[i];
+        // console.log(max);
+    // }
+// }
+
+
+
+
+
+for (let i=0;i<arr.length;i++){
+    if(arr[i]<max){
+        max=arr[i]
+    }
+    }
+console.log(max);
