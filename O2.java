2e2f63bcf26d6d4cc172268935ad70d7cfa0e27e
KANITA VERMA <vermakanita13@gmail.com>
2025-02-10 20:58:28 +0530

    Add files via upload

---
+++ O2.java
@@ -0,0 +1,24 @@
+package TestProblems;
+
+public class O2 {
+    public static void main(String str[]){
+        int arr[]= {2,4,6,7,1};
+        boolean yes = true;
+        boolean no = false;
+
+        for(int i=0; i<5;i++){
+            if(arr[i]==6){
+                System.out.println(boolean a=true);
+                System.out.println(yes);
+                System.out.println(arr[i]);
+                // break;
+            }
+            // else{
+            //     System.out.println("no");
+                
+           // }
+        }
+        
+        
+    }
+}
