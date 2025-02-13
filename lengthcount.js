4cb0ffb78465b9532686f1567378d046835cbd34
KANITA VERMA <vermakanita13@gmail.com>
2025-02-13 22:01:48 +0530

    Add files via upload

---
+++ lengthcount.js
@@ -0,0 +1,22 @@
+// let s = "kanita";
+// let c=0;
+// let sum="";
+// // console.log(s.length)
+// for(let i=0;i<s.length;i++){
+//     sum =sum+ s[i];
+//     c++
+// }
+// console.log(c)
+// console.log(sum)
+
+
+
+
+
+let s = "kanita" ;
+sum=0
+ for(let i=0;i<s.length;i++){
+    sum=sum+s.charCodeAt(i)
+    // console.log(sum)
+ }
+ console.log(sum)
