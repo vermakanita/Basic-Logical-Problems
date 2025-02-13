e16ce3f710268eb3ce107e3e5ab76eea0cc85c44
KANITA VERMA <vermakanita13@gmail.com>
2025-02-13 21:59:42 +0530

    Add files via upload

--- Duplicate.js
+++ Duplicate.js
@@ -1,14 +1,21 @@
 let arr=[1,2,3,2,1,1,2];
 let arr1=[];
-let arr2=[];
+// let arr2=[];
 for(let i=0;i<arr.length;i++){
-    if(!arr1==arr[i]){
+    if(!arr1.includes(arr[i])){
         arr1.push(arr[i])
     // console.log("k")
     }
-    // else{
-    //     arr2.push(arr[i])
-    // }
+    else{
+        // arr2.push(arr[i])
+        console.log(arr[i]);
+    }
 }
 console.log(arr1);
-console.log(arr2)
+// console.log(arr2)
+
+
+// let arr = [1, 2, 3, 2, 1, 1, 2];
+// let arr1 = [...new Set(arr)];
+
+// console.log(arr1); // Output: [1, 2, 3]
