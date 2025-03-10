cce3b0eacd6a0c53f605366a717f26bfb633da69
KANITA VERMA <vermakanita13@gmail.com>
2025-03-11 00:01:13 +0530

    Add files via upload

---
+++ map.js
@@ -0,0 +1,12 @@
+//it collection of key value paire maintaint order 
+// key and value can be any type
+const maps = new Map();
+maps.set("name","kanita");
+maps.set("age",22);
+console.log(maps.get("age"));
+console.log(maps.has("age"));
+console.log(maps.delete("age"));
+console.log(maps);
+
+
+
